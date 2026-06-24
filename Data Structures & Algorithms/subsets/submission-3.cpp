5668118e08493cf4e1fcf27db3dbe345be88aea0
Sandeep V Kollipara <46266688+sandeep-kollipara@users.noreply.github.com>
2026-06-24 11:47:42 -0400

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        auto backtrack=[](auto&& self, int i, vector<int>& temp, vector<int>& arr, vector<vector<int>>& subsets){
+            // base case: work and termination at leaves
+            if (i==arr.size()) {
+                subsets.push_back(temp);
+                return ;
+            }
+            // traversal
+            temp.push_back(arr[i]);
+            self(self, i+1, temp, arr, subsets);
+            temp.pop_back();
+            while (i+1<arr.size() and arr[i+1]==arr[i]) i++; 
+            self(self, i+1, temp, arr, subsets);
+        };
+        vector<int> nullset;
+        vector<vector<int>> subs;
+        sort(nums.begin(), nums.end());
+        backtrack(backtrack, 0, nullset, nums, subs);
+        return subs;
+    }
+};
