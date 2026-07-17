ab9da47844dbb126cb8a220de09341b8e00c4b61
Sandeep V Kollipara <46266688+sandeep-kollipara@users.noreply.github.com>
2026-07-17 18:32:03 -0400

    Add: strobogrammatic-number - submission-3

---
+++ Data Structures & Algorithms/strobogrammatic-number/submission-3.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isStrobogrammatic(string num) {
+        unordered_map<char,char> hashmap;
+        hashmap['0'] = '0';
+        hashmap['1'] = '1';
+        hashmap['6'] = '9';
+        hashmap['9'] = '6';
+        hashmap['8'] = '8';
+        hashmap['2'] = '-';
+        hashmap['3'] = '-';
+        hashmap['4'] = '-';
+        hashmap['5'] = '-';
+        hashmap['7'] = '-';
+        int i=0, j=num.length()-1;
+        while (i<j) {
+            if (hashmap[num[i]]!=num[j]) return false;
+            i++;
+            j--;
+        }
+        if (num.length()%2 and num[i]!='0' and num[i]!='1' and num[i]!='8') return false;
+        return true;
+    }
+};
